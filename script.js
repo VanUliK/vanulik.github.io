380579b9d5dba35b63f0de48184f75ee968dd98f
VanUliK <m.vanulik@gmail.com>
2022-10-20 21:28:18 +0200

    solution dz

---
+++ script.js
@@ -0,0 +1,53 @@
+// Создать страницу, которая спрашивает имя у пользователя и выводит его с помощью функции.
+
+const result = prompt("Как тебя зовут?", "");
+alert(`Привет, ${result}`);
+
+// if (confirm("Вы уверены?")) {
+//   alert("Мы рады, что Вы уверены!");
+// } else {
+//   alert("Жаль, что Вы не уверены...");
+// }
+
+// const result = prompt("Hello", "default_name");
+// alert(result);
+
+// if (confirm("Вы уверены?")) {
+//   alert("Мы рады, что Вы уверены!");
+// } else {
+//   alert("Жаль, что Вы не уверены...");
+// }
+
+// const max = {
+//   name: "Ivan",
+//   age: 35,
+//   profession: "bezdelnik",
+//   city: "Kaliningrad",
+// };
+
+// const maxCity = {
+//   City: 2,
+//   Moscow: 3,
+//   Piter: 12,
+// };
+
+// const years = Object.values(maxCity);
+// function sum(arr) {
+//   let sum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+
+//   return sum;
+// }
+// alert(`${sum(years)}`);
+
+// const array = [];
+// array.length = Math.floor(Math.random() * 10);
+
+// for (let i = 0; i < array.length; i++) {
+//   array[i] = Math.floor(Math.random() * 10);
+// }
+// const m = array.length - 1;
+// alert(`${array[m]}`);
