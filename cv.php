8951f6b931eaf81fd24a75781f7f21556c06a824
VanUliK <m.vanulik@gmail.com>
2022-11-09 21:38:28 +0200

    reload

--- cv.php
+++ cv.php
@@ -1,24 +1,26 @@
 <?php
 
-$name = "Иван";
-$city = "Калининград";
-$profession = "Инженер";
-$email = "ivan@maill.ru";
-$phone = 89091234567;
-$array = [
-     'array_name' => ['Готовка', 'Электроника', 'Media', 'Программирование'],
-   'level' => [90, 70, 50, 60]
- ];
- 
-$age_year = 2022-1987; //возраст в годах
-$age_day = (2022-1987)*365; //возраст в днях
+$name = 'Ivan';
+$prof = 'support';
+$city = 'Kaliningrad';
+$email = 'Ivan@mail.ru';
+$tel = 89879875465;
+$skills = [
+  'skil_name' => ['Первый', 'Второй', 'Третий', 'Четвертый'],
+  'level' => [100, 23, 41, 52]
+];
 
-$work = [
-      'work' => ['Хлебозавод ЗАО «Типек»', 'Кирпичный завод ООО «Пятый элемент»', 'Россети Янтарь'],
-      'year' => ['Июль 2007г. – Август 2014г', 'Август 2014г. – Октябрь 2014г', 'Октябрь 2014г. – По настоящее время']
- ];
- 
+$god = 2022;
+$years = $god - 1987;
+$years_day = ($god - 1987) * 365;
+$yer = 1;
+if ($god > 1987) {
+  $yer = $god / 4;
+}
+
+
 ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -31,6 +33,7 @@ $work = [
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
   <style>
     html,
     body,
@@ -58,119 +61,104 @@ $work = [
 
         <div class="w3-white w3-text-grey w3-card-4">
           <div class="w3-display-container">
-            <img src="https://imgfon.ru/Images/Details_img_880px/Animals/glaza-belyy_fon-kotik-morda-polosatyy.webp"
-              style="width:100%" alt="Avatar">
+            <img src="https://imgfon.ru/Images/Details_img_880px/Animals/glaza-belyy_fon-kotik-morda-polosatyy.webp" style="width:100%" alt="Avatar">
             <div class="w3-display-bottomleft w3-container w3-text-black">
               <h2>
                 <?php
-         echo $name;
-         ?>
+                echo $name;
+                ?>
               </h2>
             </div>
           </div>
           <div class="w3-container">
-            <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>
-              <?php echo $profession;?>
+            <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Профессия: <?php
+                                                                                                      echo $prof;
+                                                                                                      ?>
+              </h2>
             </p>
-            <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>
+            <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Город:
               <?php
-         echo $city;
-         ?>
-            </p>
-            <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>
+              echo $city;
+              ?></p>
+            <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>E-mail:
               <?php
-         echo $email;
-         ?>
+              echo $email;
+              ?>
             </p>
-            <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>
+            <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>Телефон:
               <?php
-         echo $phone;
-         ?>
+              echo $tel;
+              ?>
             </p>
-            <hr>
-            <p class="w3-large w3-text-theme"><b><i
-                  class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Возраст</b></p>
-            <p>В днях</p>
-            <div class="w3-light-grey w3-round-xlarge">
-              <div class="w3-round-xlarge w3-teal" style="text-align: center;">
-                <?php
-              echo $age_day;
-            ?>
-              </div>
-            </div>
-            <p>В годах</p>
-            <div class="w3-light-grey w3-round-xlarge">
-              <div class="w3-round-xlarge w3-teal" style="text-align: center;">
-                <?php
-              echo $age_year;
-            ?>
-              </div>
-            </div>
+            <p><i class="fa fa-hourglass fa-fw w3-margin-right w3-large w3-text-teal"></i>Возраст:
+              <?php
+              echo "$years лет $years_day дней $yer"
+              ?>
 
-            <br>
+            </p>
+
+            <hr>
+
             <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Навыки</b></p>
             <p>
               <?php
-   echo $array['array_name'][0];
-   ?>
+              echo $skills['skil_name'][0];
+              ?>
             </p>
             <div class="w3-light-grey w3-round-xlarge w3-small">
-              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:<?php
-   echo $array['level'][0];
-   ?>%">
-                <?php
-   echo $array['level'][0];
-   ?>%
-              </div>
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:
+              <?php
+              echo $skills['level'][0];
+              ?>%"><?php
+                    echo $skills['level'][0];
+                    ?>%</div>
             </div>
             <p>
               <?php
-   echo $array['array_name'][1];
-   ?>
+              echo $skills['skil_name'][1];
+              ?>
             </p>
             <div class="w3-light-grey w3-round-xlarge w3-small">
-              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:<?php
-   echo $array['level'][1];
-   ?>%">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:
+              <?php
+              echo $skills['level'][1];
+              ?>%">
                 <div class="w3-center w3-text-white">
                   <?php
-   echo $array['level'][1];
-   ?>%
-                </div>
+                  echo $skills['level'][1];
+                  ?>%</div>
               </div>
             </div>
             <p>
               <?php
-   echo $array['array_name'][2];
-   ?>
+              echo $skills['skil_name'][2];
+              ?>
             </p>
             <div class="w3-light-grey w3-round-xlarge w3-small">
-              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:<?php
-   echo $array['level'][2];
-   ?>%">
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:
+              <?php
+              echo $skills['level'][2];
+              ?>%">
                 <?php
-   echo $array['level'][2];
-   ?>%
-              </div>
+                echo $skills['level'][2];
+                ?>%</div>
             </div>
             <p>
               <?php
-   echo $array['array_name'][3];
-   ?>
+              echo $skills['skil_name'][3];
+              ?>
             </p>
             <div class="w3-light-grey w3-round-xlarge w3-small">
-              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:<?php
-   echo $array['level'][3];
-   ?>%">
-                <?php
-   echo $array['level'][3];
-   ?>%
-              </div>
+              <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:
+              <?php
+              echo $skills['level'][3];
+              ?>%"><?php
+                    echo $skills['level'][3];
+                    ?>%</div>
             </div>
             <br>
 
-            <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Языки</b>
-            </p>
+            <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Языки</b></p>
             <p>Английский</p>
             <div class="w3-light-grey w3-round-xlarge">
               <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
@@ -184,12 +172,8 @@ $work = [
               <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%"></div>
             </div>
             <br>
-
-
           </div>
         </div><br>
-
-
 
         <!-- End Left Column -->
       </div>
@@ -198,55 +182,28 @@ $work = [
       <div class="w3-twothird">
 
         <div class="w3-container w3-card w3-white w3-margin-bottom">
-          <h2 class="w3-text-grey w3-padding-16"><i
-              class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
           <div class="w3-container">
-            <h5 class="w3-opacity"><b>
-                <?php
-              echo $work['work'][2];
-              ?>
-              </b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
-              <?php
-              echo $work['year'][2];
-              ?>
-            </h6>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <h5 class="w3-opacity"><b>Front End Developer </b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
+            <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
             <hr>
           </div>
           <div class="w3-container">
-            <h5 class="w3-opacity"><b>
-                <?php
-              echo $work['work'][1];
-              ?>
-              </b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
-              <?php
-              echo $work['year'][1];
-              ?>
-            </h6>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            <h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
+            <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
             <hr>
           </div>
           <div class="w3-container">
-            <h5 class="w3-opacity"><b>
-                <?php
-              echo $work['work'][0];
-              ?>
-              </b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>
-              <?php
-              echo $work['year'][0];
-              ?>
-            </h6>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p><br>
+            <h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
           </div>
         </div>
 
         <div class="w3-container w3-card w3-white">
-          <h2 class="w3-text-grey w3-padding-16"><i
-              class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
           <div class="w3-container">
             <h5 class="w3-opacity"><b>gb.ru</b></h5>
             <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
