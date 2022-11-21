bb65e0d4a8f5c1ba10293ea4965214857353cf7c
VanUliK <m.vanulik@gmail.com>
2022-11-21 20:00:59 +0200

    add figma

---
+++ figma/scripts/script.js
@@ -0,0 +1,27 @@
+// document.getElementsByClassName( "main-title" )[0].style.color = "red";
+document.getElementById("main-action").onclick = function () {
+    document.getElementById("cars").scrollIntoView({ behavior: "smooth" });
+}
+
+var buttons = document.getElementsByClassName("car-button");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("price").scrollIntoView({ behavior: "smooth" });
+
+    }
+}
+
+document.getElementById("price-action").onclick = function () {
+    if (document.getElementById("name").value === "") {
+        alert("Заполните поле имя!")
+    }
+    else if (document.getElementById("phone").value === "") {
+        alert("Заполните поле телефон!")
+    }
+    else if (document.getElementById("car").value === "") {
+        alert("Заполните поле автомобиль!")
+    }
+    else {
+        alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время");
+    }
+}
