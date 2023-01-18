1eb07e86467f9b333a8e32cf64aa381eb0d9abda
VanUliK <m.vanulik@gmail.com>
2023-01-18 23:14:13 +0200

    reload

--- JS/lesson05/task03.js
+++ JS/lesson05/task03.js
@@ -9,6 +9,7 @@ const obj = {
     key1: 4,
     key2: 5,
     key3: 6,
+
   },
   key3: {
     key1: 7,
@@ -18,11 +19,31 @@ const obj = {
 }
 
 
-console.log(obj);
+const test1 = Object.values(obj.key1);
 let sum = 0;
-for (let key in obj.key2) {
-  sum += Number(obj[obj.key2]);
-
-  console.log(sum);
+for (let i = 0; i < test1.length; i++) {
+  sum += test1[i];
 }
+const test2 = Object.values(obj.key2);
+for (let i = 0; i < test2.length; i++) {
+  sum += test2[i];
+}
+
+const test3 = Object.values(obj.key3);
+for (let i = 0; i < test3.length; i++) {
+  sum += test3[i];
+}
+
+console.log(sum);
+
+
+
+
+// console.log(obj);
+// let sum = 0;
+// for (let key in obj.key2) {
+//   sum += Number(obj[obj.key2]);
+
+//   console.log(sum);
+// }
 // console.log(sum);
