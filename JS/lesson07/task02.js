ddbe22c95583b989f83f8a9d8016c517b98caf7e
VanUliK <vanulik@work.com>
2023-01-24 15:58:04 +0200

    add lesson07

---
+++ JS/lesson07/task02.js
@@ -0,0 +1,15 @@
+
+// Дан код
+// <div class="elem">
+//   <img src="photo.png" alt="photo">
+//   <div class="content">
+//     <h2 class="heading">Lorem, ipsum dolor.</h2>
+//     <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, ea!</p>
+//   </div>
+// </div>
+
+// Необходимо с помощью querySelector найти параграф с классом text
+// Вывести в консоль соседний элемент h2
+// Вывести в консоль родительский элемент content
+// Вывести в консоль картинку
+// Вывести в консоль родительский элемент elem
