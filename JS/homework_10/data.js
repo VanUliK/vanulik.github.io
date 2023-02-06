c62c3e93014fc63582beee5d21dbc896515894ff
VanUliK <m.vanulik@gmail.com>
2023-02-06 22:01:25 +0200

    solution homework10

--- JS/homework_10/data.js
+++ JS/homework_10/data.js
@@ -64,5 +64,68 @@ const dataApi = `[
     "active": "1",
     "comment": "",
     "image": "https://bing.ioliu.cn/v1/rand"
+  },{
+    "number": "2241",
+    "language": "en",
+    "insult": "The tartness of your face sours ripe grapes.",
+    "created": "2023-02-06 21:00:54",
+    "shown": "3360",
+    "createdby": "No Sweat Shakespeare",
+    "active": "1",
+    "comment": "The Comedy of Erros (Act 5, Scene 4)",
+    "image": "https://bing.ioliu.cn/v1/rand"
+  },
+  {
+    "number": "2256",
+    "language": "en",
+    "insult": "Thou leathern-jerkin, crystal-button, knot-pated, agatering, puke-stocking, caddis-garter, smooth-tongue, Spanish pouch!",
+    "created": "2023-02-06 21:17:03",
+    "shown": "3303",
+    "createdby": "No Sweat Shakespeare",
+    "active": "1",
+    "comment": "Henry IV Part 1 (Act 2, Scene 4)",
+    "image": "https://bing.ioliu.cn/v1/rand"
+  },
+  {
+    "number": "2252",
+    "language": "en",
+    "insult": "Thou cream faced loon",
+    "created": "2023-02-06 21:10:03",
+    "shown": "3294",
+    "createdby": "No Sweat Shakespeare",
+    "active": "1",
+    "comment": "Macbeth (Act 5, Scene 3)",
+    "image": "https://bing.ioliu.cn/v1/rand"
+  },{
+    "number": "235",
+    "language": "en",
+    "insult": "I guess those penis enlargement pills are working, you're twice the dick you were yesterday.",
+    "created": "2023-02-06 13:59:06",
+    "shown": "3322",
+    "createdby": "",
+    "active": "1",
+    "comment": "https://imgur.com/gallery/ph1VT",
+    "image": "https://bing.ioliu.cn/v1/rand"
+  },{
+    "number": "2287",
+    "language": "en",
+    "insult": "You haven't been yourself lately. We've all noticed the improvement.",
+    "created": "2023-02-06 21:25:29",
+    "shown": "3231",
+    "createdby": "9GAG",
+    "active": "1",
+    "comment": "",
+    "image": "https://bing.ioliu.cn/v1/rand"
+  },
+  {
+    "number": "2380",
+    "language": "en",
+    "insult": "You’re so ugly, you could make a freight train take a dirt road.",
+    "created": "2023-02-06 21:43:17",
+    "shown": "3153",
+    "createdby": "",
+    "active": "1",
+    "comment": "",
+    "image": "https://bing.ioliu.cn/v1/rand"
   }
 ]`
