5ea4f468aad5118ef31f20c2f70d01e22c5543ba
VanUliK <m.vanulik@gmail.com>
2023-02-09 20:11:55 +0200

    reload

---
+++ JS/lesson11/task01/data.js
@@ -0,0 +1,18 @@
+const item = `[
+  {
+    "head": "MANGO  PEOPLE  T-SHIRT",
+    "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae quae voluptatem, cum ut enim soluta.",
+    "price": "300",
+     "color": "Red",
+     "size": "XL",
+    "image": "img/img_1.jpg"
+  },
+  {
+    "head": "ELLERY X M'O CAPSULE",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "52",
+    "color": "Black",
+    "size": "XL",
+    "image": "img/img_2.jpg"
+  }
+]`
