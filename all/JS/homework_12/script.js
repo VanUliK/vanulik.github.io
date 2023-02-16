b5d8a956d954bec60124231b5454a6148fcf53c7
VanUliK <vanulik@work.com>
2023-02-16 15:36:13 +0200

    reload

--- all/JS/homework_12/script.js
+++ all/JS/homework_12/script.js
@@ -2,11 +2,20 @@ let array = fetch("./data.json")
   .then((response) => response.json())
   .then((data) => showItem(data))
   .catch((error) => console.error(error));
-function showItem(data) {
+const main = document.querySelectorAll('.featured ');
+const clickButton = document.querySelector('.view_all');
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => showItemsBlock(data))
+  .catch((error) => console.error(error));
+
+function showItemsBlock(data) {
   const templateEl = document.querySelector('.template');
   const itemGroup = document.querySelector('.featuredItems');
 
-  data.forEach(element => {
+
+  for (let index = 0; index < 6; index++) {
+    const element = data[index];
     const itemEl = templateEl.content
       .querySelector(".item_block")
       .cloneNode(true);
@@ -30,7 +39,36 @@ function showItem(data) {
     const imgEl = itemEl.querySelector(".featuredImg");
     imgEl.src = element.image;
     itemGroup.appendChild(itemEl)
+  };
+
+  clickButton.addEventListener('click', function (e) {
+    const templateEl = document.querySelector('.template');
+    const itemGroup = document.querySelector('.featuredItems');
+
+    for (let index = 6; index < 12; index++) {
+      const element = data[index];
+      const itemEl = templateEl.content
+        .querySelector(".item_block")
+        .cloneNode(true);
+
+      const featuredItemAttribute = itemEl.querySelector(".featuredItem");
+      featuredItemAttribute.setAttribute('data-id', element.id);
+      featuredItemAttribute.setAttribute('data-name', element.head);
+      featuredItemAttribute.setAttribute('data-price', element.price);
+      const headingEl = itemEl.querySelector(".featuredName");
+      headingEl.textContent = element.head;
+      const dscItem = itemEl.querySelector(".featuredText");
+      dscItem.textContent = element.description;
+      const priceEl = itemEl.querySelector(".featuredPrice");
+      const redSize = priceEl.querySelector('.featuredPrice_color');
+      redSize.textContent = '$' + element.price.toFixed(2);
+      const imgEl = itemEl.querySelector(".featuredImg");
+      imgEl.src = element.image;
+      itemGroup.appendChild(itemEl)
+      clickButton.classList = 'hidden'
+    };
   });
+
 }
 
 const mainEl = document.querySelector('.main');
